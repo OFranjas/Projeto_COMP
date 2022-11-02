e5758861d0e07fa49adf58c6ceba272be0d0dbca
OFranjas <ofranjas@gmail.com>
2022-11-02 20:24:23 +0000

    comp

--- DEV/Meta2/compiler.sh
+++ DEV/Meta2/compiler.sh
@@ -1,3 +1,10 @@
 lex ../Meta1/jucompiler.l
 yacc -d ./jucompiler.y
 cc -o jucompiler y.tab.c lex.yy.c
+
+
+lex ../Meta1/jucompiler.l
+yacc -d -v juccompiler.y		
+cc -o jucompiler y.tab.c lex.yy.c functions.c
+./juccompiler
+./juccompiler -t
